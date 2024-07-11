3b1dcca472d461fe0f11a73907e8a710f2ea9b2c
Haroon <hoorankhan123ff@gmail.com>
2024-07-11 00:44:13 -0700

    Add files via upload

---
+++ 3 name casis.ts
@@ -0,0 +1,12 @@
+let parsonName:string = "haroon";
+// // lower case
+// console.log(parsonName.toLowerCase());
+
+// upper case
+
+console.log(parsonName.toUpperCase());
+
+// title case
+
+console.log(parsonName.charAt(0).toUpperCase() + parsonName.slice(1.4));
+console.log();
