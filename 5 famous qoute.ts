173ba3cafb84a75bb01a8cbf4e2c0b38a6098432
Haroon <hoorankhan123ff@gmail.com>
2024-07-11 00:47:54 -0700

    Add files via upload

---
+++ 5 famous qoute.ts
@@ -0,0 +1,10 @@
+let famousparson :string = "Albert Einstein";
+let famousqoute :string = '"A person who never made a mistake never tried anything new."';
+
+
+// compose message
+
+let message:string =`${famousparson} one said , ${famousqoute}`
+
+console.log(message);
+
