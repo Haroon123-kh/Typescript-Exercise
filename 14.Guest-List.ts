de7f22e49229bc266df7b93e0e4eea3d16a6837c
Haroon <hoorankhan123ff@gmail.com>
2024-07-11 02:25:49 -0700

    Add files via upload

---
+++ 14.Guest-List.ts
@@ -0,0 +1,9 @@
+
+
+let Guestlist :string[] = ['Ziya khan ','Daniyal Nagori','Kamran tessori',];
+
+for(let i=0; i<Guestlist.length; i++)[
+
+console.log('Dear Mr. ' + Guestlist[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!')
+
+]
