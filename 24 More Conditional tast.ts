fbb942ea222ca3c908f7b89addd6d3be4de3f357
Haroon <hoorankhan123ff@gmail.com>
2024-07-11 08:46:11 -0700

    Add files via upload

---
+++ 24 More Conditional tast.ts
@@ -0,0 +1,34 @@
+console.log("Enequality tast:", ("Haroon khan" as string) === "Haroon khan" );
+
+console.log("Enequality tast;",("Haroon khan" as string) === "imran khan" );
+
+
+// lowercase tast
+
+console.log("Lowercase tast:" , ".Haroon khaN". toLowerCase() === "Haroon khan")
+
+
+
+// Numerical tast
+
+console.log("Equality tast:", 1000 === 1000) 
+console.log("Inequality tast", (10 as number) !== 9);
+console.log("greater than tast", 1000 > 100);
+console.log("less than", 10 < 100);
+console.log("greater than and equals to tast", 10 >= 10 ) ;
+console.log("less than and equals to tast", 5 <= 10);
+
+// Tast using 'and and "0r"
+console.log("And operator tast", true && true);
+console.log("or operator tast", true || false);
+
+// tast whather an
+
+const car = [1 , 2, 3];
+console.log("tast number", car.includes);
+
+// tast whether
+
+const car1 = [1 ,2 ,3];
+
+console.log("tast number", !car.includes(6));
