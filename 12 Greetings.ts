ee0978a172224f924e9e90483d0f7932dda9f6c6
Haroon <hoorankhan123ff@gmail.com>
2024-07-11 01:05:19 -0700

    Add files via upload

---
+++ 12 Greetings.ts
@@ -0,0 +1,11 @@
+
+
+let names :string[] = ['Haroon','asad','mateen','kamal'] ;
+
+const message :string = 'welcome to typescript class: '
+
+for(let i=0; i<names.length; i++){
+
+console.log(message + names[i]) ;
+
+}
